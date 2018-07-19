6b6ced673ce7c8a7d010ae29edd8175648850f58
ShawDa <1315193735@qq.com>
2018-07-19 17:26:04 +0800

    keras densenet

---
+++ 6DenseNet/densenet201.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+
+from keras.optimizers import SGD
+from keras.applications.densenet import DenseNet201
+
+from sklearn.metrics import log_loss
+
+from load_cifar10 import load_cifar10_data
+
+
+if __name__ == '__main__':
+
+    # Example to fine-tune on 3000 samples from Cifar10
+
+    img_rows, img_cols = 224, 224 # Resolution of inputs
+    channel = 3
+    num_classes = 10
+    batch_size = 16
+    nb_epoch = 10
+
+    # Load Cifar10 data. Please implement your own load_data() module for your own dataset
+    X_train, Y_train, X_valid, Y_valid = load_cifar10_data(img_rows, img_cols)
+
+    # Load our model
+    # model = densenet169_model(img_rows=img_rows, img_cols=img_cols, color_type=channel, num_classes=num_classes)
+
+    # load keras model
+    model = DenseNet201(weights=None, classes=10)
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Start Fine-tuning
+    model.fit(X_train, Y_train,
+              batch_size=batch_size,
+              epochs=nb_epoch,
+              shuffle=True,
+              verbose=1,
+              validation_data=(X_valid, Y_valid),
+              )
+
+    # Make predictions
+    predictions_valid = model.predict(X_valid, batch_size=batch_size, verbose=1)
+
+    # Cross-entropy loss score
+    score = log_loss(Y_valid, predictions_valid)
